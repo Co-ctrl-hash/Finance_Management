f7e2ec70481ee57d12a02c45ffc48ebf108f7278
co-ctrl-hash <btech10920.22@bitmesra.ac.in>
2026-04-05 15:46:37 +0530

    deploy setup

---
+++ prisma/migrations/20260405045512_init/migration.sql
@@ -0,0 +1,72 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('VIEWER', 'ANALYST', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "RecordType" AS ENUM ('INCOME', 'EXPENSE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'VIEWER',
+    "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FinancialRecord" (
+    "id" TEXT NOT NULL,
+    "amount" DECIMAL(12,2) NOT NULL,
+    "currency" TEXT NOT NULL DEFAULT 'INR',
+    "type" "RecordType" NOT NULL,
+    "category" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "notes" TEXT,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdByUserId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "FinancialRecord_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_role_idx" ON "User"("role");
+
+-- CreateIndex
+CREATE INDEX "User_status_idx" ON "User"("status");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_type_idx" ON "FinancialRecord"("type");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_category_idx" ON "FinancialRecord"("category");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_date_idx" ON "FinancialRecord"("date");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_createdByUserId_idx" ON "FinancialRecord"("createdByUserId");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_date_type_idx" ON "FinancialRecord"("date", "type");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_isDeleted_idx" ON "FinancialRecord"("isDeleted");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_createdByUserId_date_idx" ON "FinancialRecord"("createdByUserId", "date");
+
+-- AddForeignKey
+ALTER TABLE "FinancialRecord" ADD CONSTRAINT "FinancialRecord_createdByUserId_fkey" FOREIGN KEY ("createdByUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
